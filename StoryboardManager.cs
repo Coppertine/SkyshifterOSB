69f5f96ca83842be5bcfb8dffe2fcf7e68f52912
Coppertine <37494321+Coppertine@users.noreply.github.com>
2018-07-25 21:28:08 +1000

    Debug Objects

---
+++ StoryboardManager.cs
@@ -0,0 +1,55 @@
+using OpenTK;
+using OpenTK.Graphics;
+using StorybrewCommon.Mapset;
+using StorybrewCommon.Scripting;
+using StorybrewCommon.Storyboarding;
+using StorybrewCommon.Storyboarding.Util;
+using StorybrewCommon.Subtitles;
+using StorybrewCommon.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StorybrewScripts
+{
+    public class StoryboardManager : StoryboardObjectGenerator
+    {
+        [Configurable] public string gitSpritesPath;
+        [Configurable] public bool Refresh;
+        public override void Generate()
+        {
+            ImportAssets();
+        }
+        void ImportAssets()
+        {
+            //set the direction Path to copy assets
+            string dirPath = MapsetPath + "/sb/sprites";
+            //Check if the direction path exist, if not create a new one and restart the function
+            if(System.IO.Directory.Exists(dirPath))
+            {
+                string[] files = System.IO.Directory.GetFiles(gitSpritesPath);
+                //List each elements of the gitrepo sprite folder then get their file name and copy them in the destination folder
+                foreach(var file in files)
+                {
+                    var fileName = System.IO.Path.GetFileName(file);
+                    var destFile = System.IO.Path.Combine(dirPath, fileName);
+                    //Check if the file is already existing in the mapset sprite folder, if yes it copy the file!
+                    if(!System.IO.File.Exists(destFile))
+                    {
+                        System.IO.File.Copy(file, destFile, true);
+                        Log("Added " + fileName + " To " + dirPath);
+                    } 
+                }
+            }
+            else
+            {
+                System.IO.Directory.CreateDirectory(dirPath);
+                Log("Created sprite directory in " + dirPath);
+                ImportAssets();
+                return;
+            }
+            //After every instructions are done :)
+            Log("Done! (" + System.DateTime.Now.TimeOfDay + ")");
+        }
+    }
+}
