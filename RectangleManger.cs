64120680e9b1b6bc075816ac2e1899ec581f92ce
PoNo <lopzerk@hotmail.fr>
2018-07-27 11:41:22 +0200

    Update Intro

--- RectangleManger.cs
+++ RectangleManger.cs
@@ -16,29 +16,31 @@ namespace StorybrewScripts
     {
         public override void Generate()
         {
-		    GenerateRectangle(1000, 10000, new Vector2(320, 240), 200, 100, 10, 10);
+		    GenerateSplittedRectangle(4863, 14746, new Vector2(320, 240), 300, 50, 30, 10, Color4.Black, 0.75, 20);
             
         }
-
-        private void GenerateRectangle(int startTime, int endTime, Vector2 position, int scaleX, int scaleY, int xElements, int yElements)
+        private void GenerateSplittedRectangle(int startTime, int endTime, Vector2 position, int scaleX, int scaleY, int xElements, int yElements, Color4 color, double fade, float speed)
         {
+
             var startPosX = position.X - scaleX/2;
             var startPosY = position.Y - scaleY/2;
 
             var elementScaleX = scaleX/xElements;
             var elementScaleY = scaleY/yElements;
 
-            var delayMultiplier = 80f;
-            var maxDelay = Math.Abs(xElements + yElements) * delayMultiplier; //used to invert our function
+            var delayMultiplier = speed;
+            var maxDelay = Math.Abs(xElements /2f + yElements /2f) * delayMultiplier; //used to invert our function
+            var offset = new Vector2(scaleX /2f, scaleY /2f);
 
             for (int x = 0; x < xElements; x++) {
                 for (int y = 0; y < yElements; y++) {
-                    var sTime = maxDelay - Math.Abs(x - xElements / 2f + y - yElements / 2f) * delayMultiplier; //get the startTime of the indiviual rect
+                    var sTime = startTime + maxDelay - Math.Abs(x - xElements / 2f + y - yElements / 2f) * delayMultiplier; //get the startTime of the indiviual rect
                     
-                    var sprite = GetLayer("Rectangle").CreateSprite("sb/pixel.png", OsbOrigin.TopLeft, new Vector2(x * elementScaleX, y * elementScaleY) + position);
-                    sprite.Fade(sTime, sTime + 300, 0, 0.5f);
-                    sprite.Fade(endTime, 1);
+                    var sprite = GetLayer("Rectangle").CreateSprite("sb/pixel.png", OsbOrigin.TopLeft, new Vector2(x * elementScaleX, y * elementScaleY) + position - offset);
+                    sprite.Fade(sTime, sTime + 300, 0, fade);
+                    sprite.Fade(endTime, endTime + 1000, fade, 0);
                     sprite.ScaleVec(sTime, elementScaleX, elementScaleY);
+                    sprite.Color(startTime, color);
                 }
             }
         }
