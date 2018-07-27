9870354990068bbf662bff6a08a27db8ae7267a2
PoNo <lopzerk@hotmail.fr>
2018-07-27 18:45:26 +0200

    MovingParticle Update

--- MovingParticle.cs
+++ MovingParticle.cs
@@ -22,18 +22,35 @@ namespace StorybrewScripts
             {
                 if(hitobject.StartTime == 27452)
                 {
+                    for(int i = 0; i < 50; i++)
+                    {
+                        var particleStartPosition = generateRadianPositions(100, 300, hitobject.Position);
+                        var particleStart = GetLayer("ParticleMoving").CreateSprite("sb/dot.png", OsbOrigin.Centre, particleStartPosition);
+                        particleStart.Move(OsbEasing.InExpo, 24628, hitobject.StartTime, particleStartPosition, hitobject.Position);
+                        particleStart.Scale(hitobject.StartTime, Random(0.05, 0.1));
+                        particleStart.Fade(Random(24628, 26040), 26040, 0, 1);
+                    }
+
+
                     var sprite = GetLayer("ParticleMoving").CreateSprite("sb/flashydot.png", OsbOrigin.Centre, hitobject.Position);
                     sprite.Scale(hitobject.StartTime, 0.08);
                     var timestep = Beatmap.GetTimingPointAt((int)hitobject.StartTime).BeatDuration / 4;
                     var startTime = hitobject.StartTime;
                     while (true)
                     {
+                       
                         var endTime = startTime + timestep;
 
                         var complete = hitobject.EndTime - endTime < 5;
                         if (complete) endTime = hitobject.EndTime;
 
                         var startPosition = sprite.PositionAt(startTime);
+                        var particlePosition = generateRadianPositions(0, 10, startPosition);
+
+                        var particle = GetLayer("ParticleMoving").CreateSprite("sb/dot.png", OsbOrigin.Centre, particlePosition);
+                        particle.Fade(startTime, startTime + Random(1000, 2000), 1, 0);
+                        particle.Scale(startTime, Random(0.05, 0.2));
+                        particle.Color(startTime, Color4.Orange);
                         sprite.Move(startTime, endTime, startPosition, hitobject.PositionAtTime(endTime));
 
                         if (complete) break;
@@ -42,5 +59,16 @@ namespace StorybrewScripts
                 }
             }
         }
-    }
+        private Vector2 generateRadianPositions(float minRadius, float maxRadius, Vector2 centre)
+        {
+            var radius = Random(minRadius, maxRadius);
+            var radian = Random(0, Math.PI*2);
+
+            return  new Vector2
+            (
+                (float)(radius * Math.Cos(radian) + centre.X),
+                (float)(radius * Math.Sin(radian) + centre.Y)
+            );
+        }
+    } 
 }
