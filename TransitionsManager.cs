70b979abb46418d41bfd9eeb48d6c0a02d9b9992
Unknown <nick_bishop1@outlook.com>
2018-09-19 00:59:30 +1000

    Included some transitions for second verse

--- TransitionsManager.cs
+++ TransitionsManager.cs
@@ -16,8 +16,11 @@ namespace StorybrewScripts
     {
         public override void Generate()
         {
-		    TransitionStore(48628, 50040, 8);
-            
+		      TransitionStore(48628, 50040, 8);
+          
+          
+          GenerateFlare(212393, 213805);
+          GenerateDiagSplit(213805, 214158);
         }
 
         private void TransitionStore(int startTime, int endTime, int storeNumber)
@@ -39,5 +42,35 @@ namespace StorybrewScripts
                 posX += storeWidth;
             }
         }
+        
+        void GenerateFlare(double startTime, double endTime)
+        {
+            var flare = GetLayer("Transistions").CreateSprite("sb/highlight.png");
+            flare.Scale(OsbEasing.Out, startTime, endTime, 0.05, 9);
+            flare.Fade(OsbEasing.Out, startTime, endTime - 200, 0, 1);
+            flare.Fade(endTime, 0);
+        }
+        
+        void GenerateDiagSplit(double startTime, double endTime)
+        {
+            //Side1 = Top to bottom right
+            //Side2 = Bottom Right to Top
+            var side1 = GetLayer("Transitions").CreateSprite("sb/pixel.png", OsbOrigin.BottomRight, new Vector2(747, 490));
+            var side2 = GetLayer("Transitions").CreateSprite("sb/pixel.png", OsbOrigin.BottomLeft, new Vector2(-400, 410));
+            
+            side1.ScaleVec(OsbEasing.Out,startTime, endTime, new Vector2(1200, 500), new Vector2(0, 500));
+            side2.ScaleVec(OsbEasing.Out,startTime, endTime, new Vector2(1200, -500), new Vector2(0, -500));
+            
+            side1.Rotate(startTime, Math.Atan2(9, 16));
+            side2.Rotate(startTime, Math.Atan2(9, 16));
+            
+        }
+        
+        public double ConvertToRadians(double angle)
+        {
+            return (Math.PI / 180) * angle;
+        }
+        
+        
     }
 }
